025b88bf389187c22c5c28b9d7914e4642646aa5
Lucas Felix <lucario@pop-os.localdomain>
2021-06-12 17:55:33 -0300

    Configurando CORS

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ dotenv.config();
 import './database';
 
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
@@ -21,6 +23,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors());
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
